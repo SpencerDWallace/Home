d53b1e40c0c87fa719d0478d4fad66decd1c60a9
SpencerDWallace <007463307@coyote.csusb.edu>
2022-02-16 23:28:18 -0800

    added sidebar to main, with mobile query

--- main.js
+++ main.js
@@ -141,3 +141,29 @@ const init = async() => {
 }
 
 init();
+
+const sideMenuButton = document.querySelector('.topnav_sandwich');
+const sideMenu = document.querySelector('.sidemenu')
+const sideMenuOpenSymbol = sideMenuButton.textContent;
+const sideMenuExitSymbol = document.querySelector('#exit-symbol').textContent;
+const navbar = document.querySelector('.topnav-container');
+let sideMenuOpen = false;
+
+sideMenuButton.addEventListener('click', (e)=>{
+    (sideMenuOpen) ? sideMenuOpen = false: sideMenuOpen = true;
+    let pixels;
+    if(sideMenuOpen){
+        navbar.classList.add('is-hidden');
+        pixels = -1*sideMenu.getBoundingClientRect().left;
+    }
+    else{
+        navbar.classList.remove('is-hidden');
+        pixels = 0;
+    }
+    sideMenu.style.transform = 'translateX(' + pixels  + 'px)';
+    updateSideMenuButton();
+})
+
+const updateSideMenuButton = ()=>{
+    (sideMenuOpen) ? sideMenuButton.textContent = sideMenuExitSymbol : sideMenuButton.textContent = sideMenuOpenSymbol;
+}

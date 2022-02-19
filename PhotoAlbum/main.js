765a00dca878e29c4738e6b7758acf7134f66028
SpencerDWallace <007463307@coyote.csusb.edu>
2022-02-19 00:15:08 -0800

    slider

--- PhotoAlbum/main.js
+++ PhotoAlbum/main.js
@@ -3,6 +3,7 @@ const summer21AlbumButton = document.querySelector('#summer21');
 const photoCounterDOM = document.querySelector('#photo-counter');
 const nextButton = document.querySelector('#photo-button--right');
 const prevButton = document.querySelector('#photo-button--left');
+let photoSlider = document.querySelector('.range-slider__range');
 
 let currentAlbum = {};
 
@@ -23,11 +24,19 @@ let summer21 = {
 //initial state
 currentAlbum = summer20;
 let albumPath;
+photoSlider.min = 1;
+photoSlider.max = currentAlbum.numPhotos;
+photoSlider.value = currentAlbum.currentPhoto;
+document.querySelector('.range-slider__value').textContent = currentAlbum.currentPhoto;
+
 
 const updatePhoto = ()=>{
     photoCounterDOM.innerHTML = currentAlbum.currentPhoto + "/" + currentAlbum.numPhotos;
     albumPath = currentAlbum.path + currentAlbum.currentPhoto + currentAlbum.type;
     document.querySelector('#albumPhoto').src = albumPath;
+    photoSlider.max = currentAlbum.numPhotos;
+    photoSlider.value = currentAlbum.currentPhoto + "";
+    document.querySelector('.range-slider__value').textContent = currentAlbum.currentPhoto;
 }
 
 updatePhoto();
@@ -116,3 +125,28 @@ $( window ).resize(function() {
 
 // Get arbitrary element with id "my-element"
 // Listen for click events on body
+
+
+var rangeSlider = function(){
+    var slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+    slider.each(function(){
+  
+      value.each(function(){
+        var value = $(this).prev().attr('value');
+        console.log(value)
+        $(this).html(value);
+      });
+  
+      range.on('input', function(){
+        $(this).next(value).html(this.value);
+        console.log(value)
+        currentAlbum.currentPhoto = Number(photoSlider.value);
+        updatePhoto();
+      });
+    });
+
+  };
+  
+  rangeSlider();

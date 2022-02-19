4d7fdcd840878e20794184bc3cef1b40b6f2ea19
Spencer Wallace <007463307@coyote.csusb.edu>
2022-02-18 20:06:14 -0800

    overflow scroll added

--- main.js
+++ main.js
@@ -145,6 +145,7 @@ init();
 const sideMenuOpenButton = document.querySelector('.topnav_sandwich');
 const sideMenuExitButton = document.querySelector('.sidemenu-close-button');
 const sideMenu = document.querySelector('.sidemenu');
+const body = document.body;
 const navbar = document.querySelector('.topnav-container');
 let sideMenuOpen = false;
 
@@ -164,12 +165,14 @@ const slideSideMenu = (open)=>{
         navbar.classList.add('is-hidden');
         sideMenuOpenButton.classList.add('is-hidden');
         sideMenuExitButton.classList.remove('is-hidden');
+      //  body.classList.add('scroll-lock');
         pixels = -1*sideMenu.getBoundingClientRect().left;
     }
     else{
         navbar.classList.remove('is-hidden');
         sideMenuOpenButton.classList.remove('is-hidden');
         sideMenuExitButton.classList.add('is-hidden');
+      //  body.classList.remove('scroll-lock');
         pixels = 0;
     }
     sideMenu.style.transform = 'translateX(' + pixels  + 'px)';

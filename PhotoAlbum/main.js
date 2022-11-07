223c91dc19d83a52f5f2ffb1f50aab3e5c057f63
SpencerDWallace <007463307@coyote.csusb.edu>
2022-11-06 22:02:36 -0800

    fix photo slider

--- PhotoAlbum/main.js
+++ PhotoAlbum/main.js
@@ -62,3 +62,26 @@ prevButton.addEventListener('click', (e)=>{
 summer20AlbumButton.addEventListener('click', (e)=>{ updatePhotoAlbum(summer20); });
 summer21AlbumButton.addEventListener('click', (e)=>{ updatePhotoAlbum(summer21); });
 
+var rangeSlider = function(){
+    var slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+    slider.each(function(){
+  
+      value.each(function(){
+        var value = $(this).prev().attr('value');
+        console.log(value)
+        $(this).html(value);
+      });
+  
+      range.on('input', function(){
+        $(this).next(value).html(this.value);
+        console.log(value)
+        currentAlbum.currentPhoto = Number(photoSlider.value);
+        updatePhoto();
+      });
+    });
+
+  };
+  
+  rangeSlider();

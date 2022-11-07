eb17bd6be433cbca77f6b7bae03aecc01ef58206
SpencerDWallace <007463307@coyote.csusb.edu>
2022-11-06 17:27:01 -0800

    some improvements for mobile

--- main.js
+++ main.js
@@ -117,3 +117,21 @@ $( window ).resize(function() {
         }
     }
 });
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+mybutton.onclick = ()=>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}

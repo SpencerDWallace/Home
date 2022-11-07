0ba08ee2619fab166a4112df949d6a3a5ad5700e
SpencerDWallace <007463307@coyote.csusb.edu>
2022-11-06 23:20:45 -0800

    more link fixes

--- models/photoalbum-model.js
+++ models/photoalbum-model.js
@@ -9,7 +9,7 @@ internalModels.photoAlbumModel = function() {
     return [
         {
             name: 'summer 2020',
-            path: "/PhotoAlbum/summer20/",
+            path: "./summer20/",
             photos: [
                 '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png',
                 '12.png', '13.png', '14.png', '15.png', '16.png', '17.png', '18.png', '19.png', '20.png',
@@ -20,7 +20,7 @@ internalModels.photoAlbumModel = function() {
         },
         {
             name: 'summer 2021',
-            path: "/PhotoAlbum/summer21/",
+            path: "./summer21/",
             photos: [
                 '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg',
             ],
